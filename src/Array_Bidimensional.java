5e511b8855f9f1e02e38095355b33bd5f261a335
SandraCorazona <corazona0527@gmail.com>
2022-02-22 10:22:01 +0100

    Curso Java1

---
+++ src/Array_Bidimensional.java
@@ -0,0 +1,37 @@
+
+public class Array_Bidimensional {
+
+	public static void main(String[] args) {
+		
+		int [][]matriz=new int [4][5];
+		
+		matriz[0][0]=15;
+		matriz[0][1]=2;
+		matriz[0][2]=45;
+		matriz[0][3]=1;
+		matriz[0][4]=30;
+		
+		matriz[1][0]=5;
+		matriz[1][1]=47;
+		matriz[1][2]=52;
+		matriz[1][3]=63;
+		matriz[1][4]=2;
+		
+		matriz[2][0]=90;
+		matriz[2][1]=23;
+		matriz[2][2]=25;
+		matriz[2][3]=78;
+		matriz[2][4]=16;
+		
+		matriz[3][0]=36;
+		matriz[3][1]=9;
+		matriz[3][2]=29;
+		matriz[3][3]=48;
+		matriz[3][4]=10;
+		
+		System.out.println(matriz[0][3]);
+		
+
+	}
+
+}
--- src/For3.java
+++ src/For3.java
@@ -4,7 +4,7 @@ public class For3 {
 
 	public static void main(String[] args) { //Hallar factorial de un numero
 		
-		int resultado=1;
+		Long resultado=1L;
 		int numero=Integer.parseInt(JOptionPane.showInputDialog("Introduzca un número"));
 		
 		for (int i = numero; i > 0; i--) {
